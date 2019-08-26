66d7e26ee6a0dc03363fe49c3f56c9b91d343646
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-26 22:43:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
+    binding.pry
     if age < 12
     puts Happy Birthday #{kids_name}! You are now #{age} years old!"
   else
